245e225475f5456e88f26c475fb89f2df44cc3fc
JoacoGirod <joaquingirod@gmail.com>
2020-03-16 15:31:43 -0300

    Modelacion de los mecanicos (3), tercer commit

--- Smart-taller.js
+++ Smart-taller.js
@@ -1,4 +1,5 @@
 // Proyecto Smart Taller
+// Modelacion en el sistema de los autos (3)             danios >> Tren Delantero, Frenos, Bugias, Ruedas, Vidrio Trasero (5)
 var auto1 = {
     marca : "Volkswagen",
     modelo : "Gol",
@@ -16,4 +17,22 @@ var auto3 = {
     modelo : "Fiesta",
     patente : "ABE125",
     danios : ["Ruedas","Frenos","Bugias"]
+}
+// Modelacion en el sistema de los mecanicos (3)        reparaciones >> Tren Delantero, Frenos, Bugias, Ruedas, Vidrio Trasero, Tablero, Aire Acondicionado, 
+//                                                                      Vidrio trasero, Pintura y Chapa, Cableado
+var mecanico1 = {
+    nombre : "Uriel",
+    legajo : "1",
+    reparaciones : ["Bugias", "Frenos", "Tablero"]
+}
+
+var mecanico2 = {
+    nombre : "Gonzalo",
+    legajo : "2",
+    reparaciones : ["Tren delantero","Ruedas","Aire Acondicionado"]
+}
+var mecanico3 = {
+    nombre : "Ignacio",
+    legajo : "3",
+    reparaciones : ["Vidrio trasero", "Pintura y Chapa", "Cableado"]
 }
