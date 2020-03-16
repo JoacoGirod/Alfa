f6569f1a0744a2027260f0162730767fa17d587a
JoacoGirod <joaquingirod@gmail.com>
2020-03-16 15:22:40 -0300

    Modelacion de los autos (3), segundo commit

--- Smart-taller.js
+++ Smart-taller.js
@@ -1,1 +1,19 @@
 // Proyecto Smart Taller
+var auto1 = {
+    marca : "Volkswagen",
+    modelo : "Gol",
+    patente : "ABC123",
+    danios : ["Tren delantero","Frenos","Bugias"]
+}
+var auto2 = {
+    marca : "Subaru",
+    modelo : "Impreza",
+    patente : "ABD124",
+    danios : ["Ruedas","Vidrio trasero","Bugias"]
+}
+var auto3 = {
+    marca : "Ford",
+    modelo : "Fiesta",
+    patente : "ABE125",
+    danios : ["Ruedas","Frenos","Bugias"]
+}
